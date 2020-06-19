a362e8911d984fe4c5cf9232d63f1c55c36d58ea
Allan Feng <allan.feng@uconn.edu>
2020-06-19 12:45:17 -0400

    initial commit

---
+++ CryptoTracker/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by Allan Feng on 6/18/20.
+//  Copyright © 2020 Allan Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CryptoTracker/Model/coinManager.swift
@@ -0,0 +1,20 @@
+//
+//  coinManager.swift
+//  CryptoTracker
+//
+//  Created by Allan Feng on 6/18/20.
+//  Copyright © 2020 Allan Feng. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
