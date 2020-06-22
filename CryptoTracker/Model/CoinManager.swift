6d530fc79392c7c0d3d351cf2f53abb2a9ad91cb
Allan Feng <allan.feng@uconn.edu>
2020-06-22 17:03:06 -0400

    api request

--- CryptoTracker/Model/CoinManager.swift
+++ CryptoTracker/Model/CoinManager.swift
@@ -12,9 +12,30 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "D5991F1A-4AFF-4508-A17F-D30258BC2C2A"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String){
+        //add apikey to baseURL
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        if let url = URL(string: urlString){
+            //create the session
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data,response, error) in
+                if error != nil{
+                    print(error!)
+                    return
+                }
+                //convert the data to a string
+                let dataAsString = String(data: data!, encoding: .utf8)
+                print(dataAsString)
+            }
+            //start the task to fetch data
+            task.resume()
+            
+        }
+    }
 
     
 }
